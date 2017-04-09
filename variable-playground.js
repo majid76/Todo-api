c5afa13e020e589c11bd1e1d151d59e5610ef308
Majid Daghagheleh <majid.dagha@gmail.com>
2017-04-09 19:18:59 +1200

    add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+
+// };
+
+// function updatePerson(obj){
+
+// 		obj = {
+
+// 			name: 'Andrew',
+// 			age: 24
+// 		};
+
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15,88];
+function addGrade(grades){
+
+	// grades.push(55);
+	grades = [12,33,99];
+
+}
+
+addGrade(grades);
+console.log(grades);
